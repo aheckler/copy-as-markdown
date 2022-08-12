c50f4aef392d593f4bbc756aaa717d68137a951e
Adam Heckler <5512652+aheckler@users.noreply.github.com>
2022-08-12 11:31:49 -0400

    Create adams-copy-as-markdown.user.js

---
+++ adams-copy-as-markdown.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @author      aheckler
+// @description Extracts HTML from the current selection, transforms it into Markdown, and places it in the clipboard.
+// @downloadURL https://github.com/aheckler/copy-as-markdown
+// @grant       GM_setClipboard
+// @match       *://*/*
+// @name        Adam's Copy as Markdown
+// @namespace   https://github.com/aheckler/copy-as-markdown
+// @require     https://unpkg.com/turndown@^7/dist/turndown.js
+// @updateURL   https://github.com/aheckler/copy-as-markdown
+// @version     1.0.0
+// ==/UserScript==
+
+document.addEventListener( 'keydown', event => {
+  if ( ( event.ctrlKey || event.metaKey ) && event.shiftKey && event.keyCode === 67 ) {
+    copyAsMarkdown();
+    event.preventDefault(); // Stop the browser dev tools from opening.
+  }
+});
+
+function getSelectionAsHtml() {
+    let html = '';
+    const selection = window.getSelection();
+
+    if (selection.rangeCount) {
+        const container = document.createElement('div');
+        container.appendChild(selection.getRangeAt(0).cloneContents());
+        html = container.innerHTML;
+    }
+
+    return html;
+}
+
+function copyAsMarkdown() {
+    const turndownService = new TurndownService();
+    const markdown = turndownService.turndown(getSelectionAsHtml());
+    if (markdown) {
+        GM_setClipboard(markdown, 'text');
+    }
+}
